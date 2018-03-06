a91422e75241772a3a920e96730ae5c29f6270f9
chengxu <403224145@qq.com>
2018-03-06 23:37:56 +0800

    --

--- src/components/charts/studentFaceStatistics.jsx
+++ src/components/charts/studentFaceStatistics.jsx
@@ -407,7 +407,7 @@ export default class studentFaceStatistics extends React.Component {
         if(!thinkUserList){
             thinkUserList=new Array();
         }
-        var attention=this.formateNumer(_this.state.currentFaceEmotion.attention/attentionUserList.length,2);
+        var attention=this.formateNumer(_this.state.currentFaceEmotion.attention/aliveUserList.length,2);
         var confuse=this.formateNumer(confuseUserList.length/aliveUserList.length,2);
         var understand=this.formateNumer(understandUserList.length/aliveUserList.length,2);
         var thinking=this.formateNumer(thinkUserList.length/aliveUserList.length,2);
