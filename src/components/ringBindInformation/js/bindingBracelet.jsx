647c264ba3197c8ec0026121ac53d7eb20285f2f
noom <noom0130@foxmail.com>
2018-04-20 10:37:51 +0800

    .

--- src/components/ringBindInformation/js/bindingBracelet.jsx
+++ src/components/ringBindInformation/js/bindingBracelet.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Toast, InputItem, List, Radio, Icon} from 'antd-mobile';
+import {Button, Toast, InputItem, List, Radio, Icon, ListView, Card, WingBlank, WhiteSpace} from 'antd-mobile';
 import '../css/bindingBracelet.less'
 
 const RadioItem = Radio.RadioItem;
@@ -10,7 +10,13 @@ export default class bindingBracelet extends React.Component {
     constructor(props) {
         super(props);
         bindDing = this;
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        this.initData = [];
         this.state = {
+            dataSource: dataSource.cloneWithRows(this.initData),
+            defaultPageNo: 1,
             tableDivHeight: document.body.clientHeight,
             searchCheckValue: '',
             macId: '',
@@ -31,23 +37,39 @@ export default class bindingBracelet extends React.Component {
      */
     viewWatchPage(loginUser) {
         var _this = this;
+        const dataBlob = {};
+        var PageNo = this.state.defaultPageNo;
         var param = {
             "method": 'viewWatchPage',
             "aid": loginUser.ident,
             "cid": -1,
-            "pn": -1,
+            "pn": PageNo,
         };
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: function (result) {
                 if (result.msg == '调用成功' && result.success == true) {
-                    console.log(result);
                     var arr = result.response;
-                    var array = [];
-                    arr.forEach(function (v, i) {
-                        var li = <li className='liName' onClick={_this.unbindWatch.bind(this, v)}>{v.name}</li>;
-                        array.push(li);
-                    });
-                    _this.setState({array});
+                    var pager = result.pager;
+                    for (let i = 0; i < arr.length; i++) {
+                        var topic = arr[i];
+                        dataBlob[`${i}`] = topic;
+                    }
+                    var isLoading = false;
+                    if (arr.length > 0) {
+                        if (pager.pageCount == 1 && pager.rsCount < 30) {
+                            isLoading = false;
+                        } else {
+                            isLoading = true;
+                        }
+                    } else {
+                        isLoading = false;
+                    }
+                    _this.initData = _this.initData.concat(arr);
+                    _this.setState({
+                        dataSource: _this.state.dataSource.cloneWithRows(_this.initData),
+                        isLoadingLeft: isLoading,
+                        refreshing: false
+                    })
                 }
             },
             onError: function (error) {
@@ -125,7 +147,7 @@ export default class bindingBracelet extends React.Component {
         }, function (error) {
             console.log(error);
         });
-    }
+    };
 
     /**
      * 绑定
@@ -177,6 +199,7 @@ export default class bindingBracelet extends React.Component {
         };
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: function (result) {
+                console.log(result);
                 if (result.msg == '调用成功' && result.success == true) {
                     if (WebServiceUtil.isEmpty(result.response) == false) {
                         var arr = [];
@@ -226,14 +249,77 @@ export default class bindingBracelet extends React.Component {
         });
     };
 
+    /**
+     *  ListView数据全部渲染完毕的回调
+     */
+    onEndReached = (event) => {
+        var _this = this;
+        var currentPageNo = this.state.defaultPageNo;
+        if (!this.state.isLoadingLeft && !this.state.hasMore) {
+            return;
+        }
+        currentPageNo += 1;
+        this.setState({isLoadingLeft: true, defaultPageNo: currentPageNo});
+        _this.viewWatchPage(_this.state.loginUser);
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(this.initData),
+            isLoadingLeft: true,
+        });
+    };
+
     render() {
+
+        var _this = this;
+
+        const row = (rowData, sectionID, rowID) => {
+
+            return (
+                <WingBlank size="lg">
+                    <WhiteSpace size="lg"/>
+                    <Card>
+                        <Card.Header
+                            className='noomCardHeader'
+                            title={rowData.name}
+                            // thumb={rowData.bindingUser.avatar}
+                            thumb='http://60.205.86.217/upload6/2018-02-09/19/805eee4a-b707-49a2-9c75-d5b14ed9227b.jpg'
+                            extra={<span className='noomCardUnbind'
+                                         onClick={_this.unbindWatch.bind(this, rowData)}>解绑</span>}
+                        />
+                        <Card.Body>
+                            <div>{rowData.macAddress}</div>
+                        </Card.Body>
+                        <Card.Footer content={rowData.bindingUser.clazz.name}
+                                     extra={<div>{rowData.bindingUser.colAccount}</div>}/>
+                    </Card>
+                    <WhiteSpace size="lg"/>
+                </WingBlank>
+            )
+        };
+
         return (
             <div id="bindingBracelet" style={{height: document.body.clientHeight}}>
                 <div className='tableDiv' style={{height: this.state.tableDivHeight}}>
                     {/*这是列表数据,包括添加按钮*/}
-                    <ul>
-                        {this.state.array}
-                    </ul>
+                    <ListView
+                        ref={el => this.lv = el}
+                        dataSource={this.state.dataSource}    //数据类型是 ListViewDataSource
+                        renderFooter={() => (
+                            <div style={{paddingTop: 5, paddingBottom: 40, textAlign: 'center'}}>
+                                {this.state.isLoadingLeft ? '正在加载' : '已经全部加载完毕'}
+                            </div>)}
+                        renderRow={row}   //需要的参数包括一行数据等,会返回一个可渲染的组件为这行数据渲染  返回renderable
+                        className="am-list"
+                        pageSize={30}    //每次事件循环（每帧）渲染的行数
+                        //useBodyScroll  //使用 html 的 body 作为滚动容器   bool类型   不应这么写  否则无法下拉刷新
+                        scrollRenderAheadDistance={200}   //当一个行接近屏幕范围多少像素之内的时候，就开始渲染这一行
+                        onEndReached={this.onEndReached}  //当所有的数据都已经渲染过，并且列表被滚动到距离最底部不足onEndReachedThreshold个像素的距离时调用
+                        onEndReachedThreshold={10}  //调用onEndReached之前的临界值，单位是像素  number类型
+                        initialListSize={30}   //指定在组件刚挂载的时候渲染多少行数据，用这个属性来确保首屏显示合适数量的数据
+                        scrollEventThrottle={20}     //控制在滚动过程中，scroll事件被调用的频率
+                        style={{
+                            height: this.state.tableDivHeight,
+                        }}
+                    />
                     <div className='addBunton' onClick={this.addRing}>+</div>
                 </div>
                 <div className='addModel' style={{height: document.body.clientHeight}}>
--- src/components/ringBindInformation/js/boxBracelet.jsx
+++ src/components/ringBindInformation/js/boxBracelet.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Toast, InputItem, List, Radio, Icon} from 'antd-mobile';
+import {Button, Toast, InputItem, List, Radio, Icon, ListView, Card, WingBlank, WhiteSpace} from 'antd-mobile';
 import '../css/boxBracelet.less'
 
 const RadioItem = Radio.RadioItem;
@@ -10,7 +10,13 @@ export default class boxBracelet extends React.Component {
     constructor(props) {
         super(props);
         bindDing = this;
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        this.initData = [];
         this.state = {
+            dataSource: dataSource.cloneWithRows(this.initData),
+            defaultPageNo: 1,
             tableDivHeight: document.body.clientHeight,
             searchCheckValue: '',
             macId: '',
@@ -31,6 +37,12 @@ export default class boxBracelet extends React.Component {
      */
     viewAndroidBoxPage(loginUser) {
         var _this = this;
+        _this.initData.splice(0);
+        _this.state.dataSource = [];
+        _this.state.dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        const dataBlob = {};
         var param = {
             "method": 'viewAndroidBoxPage',
             "aid": loginUser.ident,
@@ -41,13 +53,27 @@ export default class boxBracelet extends React.Component {
             onResponse: function (result) {
                 if (result.msg == '调用成功' && result.success == true) {
                     var arr = result.response;
-                    var array = [];
-                    arr.forEach(function (v, i) {
-                        var li = <li className='liName'
-                                     onClick={_this.unbindWatch.bind(this, v)}>{v.bindingClazz.name}</li>;
-                        array.push(li);
-                    });
-                    _this.setState({array});
+                    var pager = result.pager;
+                    for (let i = 0; i < arr.length; i++) {
+                        var topic = arr[i];
+                        dataBlob[`${i}`] = topic;
+                    }
+                    var isLoading = false;
+                    if (arr.length > 0) {
+                        if (pager.pageCount == 1 && pager.rsCount < 30) {
+                            isLoading = false;
+                        } else {
+                            isLoading = true;
+                        }
+                    } else {
+                        isLoading = false;
+                    }
+                    _this.initData = _this.initData.concat(arr);
+                    _this.setState({
+                        dataSource: _this.state.dataSource.cloneWithRows(_this.initData),
+                        isLoadingLeft: isLoading,
+                        refreshing: false
+                    })
                 }
             },
             onError: function (error) {
@@ -225,14 +251,75 @@ export default class boxBracelet extends React.Component {
         });
     };
 
+    /**
+     *  ListView数据全部渲染完毕的回调
+     */
+    onEndReached = (event) => {
+        var _this = this;
+        var currentPageNo = this.state.defaultPageNo;
+        if (!this.state.isLoadingLeft && !this.state.hasMore) {
+            return;
+        }
+        currentPageNo += 1;
+        this.setState({isLoadingLeft: true, defaultPageNo: currentPageNo});
+        _this.viewAndroidBoxPage(_this.state.loginUser);
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(this.initData),
+            isLoadingLeft: true,
+        });
+    };
+
     render() {
+
+        var _this = this;
+
+        const row = (rowData, sectionID, rowID) => {
+
+            console.log(rowData);
+
+            return (
+                <WingBlank size="lg">
+                    <WhiteSpace size="lg"/>
+                    <Card>
+                        <Card.Header
+                            className='noomCardHeader'
+                            title={rowData.bindingClazz.name}
+                            extra={<span className='noomCardUnbind'
+                                         onClick={_this.unbindWatch.bind(this, rowData)}>解绑</span>}
+                        />
+                        <Card.Body>
+                            <div>{rowData.macAddress}</div>
+                        </Card.Body>
+                    </Card>
+                    <WhiteSpace size="lg"/>
+                </WingBlank>
+            )
+        };
+
         return (
             <div id="bindingBracelet" style={{height: document.body.clientHeight}}>
                 <div className='tableDiv' style={{height: this.state.tableDivHeight}}>
                     {/*这是列表数据,包括添加按钮*/}
-                    <ul>
-                        {this.state.array}
-                    </ul>
+                    <ListView
+                        ref={el => this.lv = el}
+                        dataSource={this.state.dataSource}    //数据类型是 ListViewDataSource
+                        renderFooter={() => (
+                            <div style={{paddingTop: 5, paddingBottom: 40, textAlign: 'center'}}>
+                                {this.state.isLoadingLeft ? '正在加载' : '已经全部加载完毕'}
+                            </div>)}
+                        renderRow={row}   //需要的参数包括一行数据等,会返回一个可渲染的组件为这行数据渲染  返回renderable
+                        className="am-list"
+                        pageSize={30}    //每次事件循环（每帧）渲染的行数
+                        //useBodyScroll  //使用 html 的 body 作为滚动容器   bool类型   不应这么写  否则无法下拉刷新
+                        scrollRenderAheadDistance={200}   //当一个行接近屏幕范围多少像素之内的时候，就开始渲染这一行
+                        onEndReached={this.onEndReached}  //当所有的数据都已经渲染过，并且列表被滚动到距离最底部不足onEndReachedThreshold个像素的距离时调用
+                        onEndReachedThreshold={10}  //调用onEndReached之前的临界值，单位是像素  number类型
+                        initialListSize={30}   //指定在组件刚挂载的时候渲染多少行数据，用这个属性来确保首屏显示合适数量的数据
+                        scrollEventThrottle={20}     //控制在滚动过程中，scroll事件被调用的频率
+                        style={{
+                            height: this.state.tableDivHeight,
+                        }}
+                    />
                     <div className='addBunton' onClick={this.addRing}>+</div>
                 </div>
                 <div className='addModel' style={{height: document.body.clientHeight}}>
