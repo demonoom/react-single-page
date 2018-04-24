33f956cee4a1f9aa6d254190dc407a1180ced7c0
noom <noom0130@foxmail.com>
2018-04-24 16:27:36 +0800

    .

--- src/components/chatSettings/js/personalSettings.jsx
+++ src/components/chatSettings/js/personalSettings.jsx
@@ -1,41 +1,17 @@
 import React from 'react';
-
-import {Icon, Grid} from 'antd-mobile';
+import {Icon} from 'antd-mobile';
 import '../css/personalSettings.less'
-
-
-// 滑动开关
-
-import {List, Switch} from 'antd-mobile';
+import {List, Switch, Toast} from 'antd-mobile';
 import {createForm} from 'rc-form';
-
-let SwitchExample = (props) => {
-    const {getFieldProps} = props.form;
-    return (
-        <List>
-            <List.Item
-                extra={<Switch
-                    {...getFieldProps('Switch8', {
-                        initialValue: true,
-                        valuePropName: 'checked',
-                    })}
-                    platform="ios"
-                    color="skyblue"
-                />}
-            >消息免打扰</List.Item>
-        </List>
-    );
-};
 
 export default class personalSettings extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            userImg:'',
-            userName:"",
-            status:1,
-            initialValue:true
+            userImg: '',
+            userName: "",
+            initialValue: true
         };
     }
 
@@ -46,46 +22,117 @@ export default class personalSettings extends React.Component {
         var searchArray = locationSearch.split("&");
         var uid = searchArray[0].split('=')[1];
         var tid = searchArray[1].split('=')[1];
-        this.getUserInfo(uid, tid);
-        
+        this.setState({uid, tid});
+        this.getUserInfo(uid);
+        this.getMessageSilenceStatus(uid, tid);
     }
 
     /**
-     * 获取登录人的信息
-     * @param uid 搜索着的id
-	 * @param tid 群id或单聊中另一个人的id
+     * 获取消息免打扰的状态
+     * @param uid
+     * @param tid
      */
-    getChatMsg(type,status){
-        if(type === 1){
-            if(this.state.initialValue){
-                this.setState({status:1});
-            }else{
-                this.setState({status:2});
-            }
-        }
-        console.log(this.state.status);
-        console.log(this.state.initialValue)
-        
-    }
-    getUserInfo(uid,tid){
+    getMessageSilenceStatus(uid, tid) {
         var _this = this;
         var param = {
-            "method": 'getUserByAccount',
-            "account": 'te' + uid,
+            "method": 'getMessageSilenceStatus',
+            "uid": uid,
+            "tid": tid,
+            "type": '1',
         };
-        WebServiceUtil.requestLittleAntApi(JSON.stringify(param),{
-            onResponse:function(result){
-                if (result.msg == '调用成功' || result.success == true) {
-                    console.log(result.response)
-                    // console.log(result.response.avatar);
-                    var userImg = <img src={result.response.avatar} alt=""/>
-                    var userName = result.response.userName;
-                    _this.setState({userImg,userName})
+        WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
+            onResponse: function (result) {
+                if (result.msg == '调用成功' && result.success == true) {
+                    _this.setState({initialValue: result.response});
                 }
             }
         })
     }
 
+    /**
+     * 获取登录人的信息
+     * @param uid 搜索着的id
+     * @param tid 群id或单聊中另一个人的id
+     */
+    getChatMsg(checked) {
+        var param = {
+            "method": 'openMessageSilence',
+            "uid": this.state.uid,
+            "tid": this.state.tid,
+            "type": '1',
+            "status": checked ? '1' : '2'
+        };
+        WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
+            onResponse: function (result) {
+                if (result.msg == '调用成功' && result.success == true) {
+
+                } else {
+                    Toast.fail(result.msg, 1);
+                }
+            }
+        })
+    }
+
+    getUserInfo(uid) {
+        var _this = this;
+        var param = {
+            "method": 'getUserByAccount',
+            "account": 'te' + uid,
+        };
+        WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
+            onResponse: function (result) {
+                if (result.msg == '调用成功' && result.success == true) {
+                    _this.buildUserInfo(result)
+                }
+            }
+        })
+    }
+
+    buildUserInfo(result) {
+        let SwitchExample = (props) => {
+            const {getFieldProps} = props.form;
+            return (
+                <List>
+                    <List.Item
+                        extra={<Switch
+                            {...getFieldProps('Switch8', {
+                                initialValue: this.state.initialValue,
+                                valuePropName: 'checked',
+                            })}
+                            platform="ios"
+                            color="skyblue"
+                            onClick={(checked) => {
+                                this.getChatMsg(checked)
+                            }}
+                        />}
+                    >消息免打扰</List.Item>
+                </List>
+            );
+        };
+        SwitchExample = createForm()(SwitchExample);
+        var userDiv = <div>
+            <div className="logoImg">
+                    <span className="imgInfo">
+                        <div className="topImg">
+                            <img src={result.response.avatar} alt=""/>
+                        </div>
+                        <span>{result.response.userName}</span>
+                    </span>
+                <img src={require("../img/addBtn.png")} className="rightBtn" onClick={this.toGroupChat}/>
+            </div>
+            <div className="searchChatMsg" onClick={this.searchChatMsg}>
+                <span className="leftText">查找聊天内容</span>
+                <span className="rightArrow">
+                        <Icon type="right"/>
+                    </span>
+            </div>
+            <div className="ingoreMsg">
+                {/* 滑动开关 */}
+                <SwitchExample/>
+            </div>
+        </div>
+        this.setState({userDiv})
+    }
 
     // 顶部加号按钮
     toGroupChat() {
@@ -103,30 +150,11 @@ export default class personalSettings extends React.Component {
             window.location.href = url;
         });
     }
-     
+
     render() {
-        SwitchExample = createForm()(SwitchExample);
         return (
             <div id="personalSettings" style={{height: document.body.clientHeight}}>
-                <div className="logoImg">
-                    <span className="imgInfo">
-                        <div className="topImg">
-                            {this.state.userImg}
-                        </div>
-                        <span>{this.state.userName}</span>
-                    </span>
-                    <img src={require("../img/addBtn.png")} className="rightBtn" onClick={this.toGroupChat}/>
-                </div>
-                <div className="searchChatMsg" onClick={this.searchChatMsg}>
-                    <span className="leftText">查找聊天内容</span>
-                    <span className="rightArrow">
-                        <Icon type="right"/>
-                    </span>
-                </div>
-                <div className="ingoreMsg">
-                    {/* 滑动开关 */}
-                    <SwitchExample onClick={this.getChatMsg}/>
-                </div>
+                {this.state.userDiv}
             </div>
         );
     }
