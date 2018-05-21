e453b367ae9437d9ec9485292825c7ae98a19906
noom <noom0130@foxmail.com>
2018-05-21 14:26:17 +0800

    .

--- src/helpers/webServiceUtil.js
+++ src/helpers/webServiceUtil.js
@@ -1,6 +1,6 @@
-var isDebug = false;
+var isDebug = true;
 var localDomain = "192.168.50.15";   //请求地址
-var isDebugLocal = false;
+var isDebugLocal = true;
 var localUrl = "192.168.50.29";    //跳转地址http://c/
 
 // //云校本地测试webService地址
