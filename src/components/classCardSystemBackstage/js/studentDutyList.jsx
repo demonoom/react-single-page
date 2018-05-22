660a934cc185160c4225699f85085ea35a389c09
john_changliang <john_changliang@163.com>
2018-05-22 10:35:26 +0800

    值日表后台默认选项设置

--- src/components/classCardSystemBackstage/js/studentDutyList.jsx
+++ src/components/classCardSystemBackstage/js/studentDutyList.jsx
@@ -12,7 +12,13 @@ export default class studentDutyList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
+            data: [{value: '1', label: '星期一'},
+                {value: '2', label: '星期二'},
+                {value: '3', label: '星期三'},
+                {value: '4', label: '星期四'},
+                {value: '5', label: '星期五'},
+                {value: '6', label: '星期六'},
+                {value: '7', label: '星期日'}],
             cols: 1,
             pickerValue: [],
             asyncValue: ['1'],
@@ -41,13 +47,7 @@ export default class studentDutyList extends React.Component {
 
     onClick = () => {
         this.setState({
-            data: [{value: '1', label: '星期一'},
-                {value: '2', label: '星期二'},
-                {value: '3', label: '星期三'},
-                {value: '4', label: '星期四'},
-                {value: '5', label: '星期五'},
-                {value: '6', label: '星期六'},
-                {value: '7', label: '星期日'}]
+
         });
     };
 
