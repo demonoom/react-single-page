7bc292f64f5977eb1ad511b9553ba7bfc098aa42
calm <calm420@163.com>
2018-05-30 21:15:58 +0800

    .

---
+++ src/components/classCardSystemBackstage/js/assessMoralEducation.jsx
@@ -0,0 +1,308 @@
+import React from 'react';
+import {
+    Toast,
+    InputItem,
+    List,
+    Radio,
+    ListView,
+    Modal,
+    PullToRefresh,
+    Checkbox,
+    Flex,
+    WhiteSpace,
+    WingBlank,
+    Button,
+} from 'antd-mobile';
+import '../css/assessMoralEducation.less'
+import { ucs2 } from 'punycode';
+
+const CheckboxItem = Checkbox.CheckboxItem;
+const AgreeItem = Checkbox.AgreeItem;
+const alert = Modal.alert;
+const RadioItem = Radio.RadioItem;
+var assessME;
+
+export default class assessMoralEducation extends React.Component {
+
+    constructor(props) {
+        super(props);
+        assessME = this;
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        this.initData = [];
+        this.state = {
+            dataSource: dataSource.cloneWithRows(this.initData),
+            defaultPageNo: 1,
+            clientHeight: document.body.clientHeight,
+            chooseResultDiv: 'none',
+            searchData: [],
+            selectData: [],
+            calmHeight: document.body.clientHeight - 150
+        };
+    }
+
+
+
+    componentDidMount() {
+        Bridge.setShareAble("false");
+        document.title = '德育教室信息列表';
+        var locationHref = window.location.href;
+        var locationSearch = locationHref.substr(locationHref.indexOf("?") + 1);
+        var clazzId = locationSearch.split("&")[0].split("=")[1];
+        assessME.setState({
+            "classId": clazzId
+        })
+        this.getMoralEducationInfoList(clazzId)
+        //添加对视窗大小的监听,在屏幕转换以及键盘弹起时重设各项高度
+        window.addEventListener('resize', assessME.onWindowResize)
+    }
+
+    componentWillUnmount() {
+        //解除监听
+        window.removeEventListener('resize', assessME.onWindowResize)
+    }
+
+    /**
+     * 视窗改变时改变高度
+     */
+    onWindowResize() {
+        setTimeout(function () {
+            moralassessMEEd.setState({
+                clientHeight: document.body.clientHeight,
+                calmHeight: document.body.clientHeight - 150
+            });
+        }, 100)
+    }
+
+    /**
+     * 查看对应教室ID的德育信息
+     */
+    getMoralEducationInfoList(classId) {
+        var _this = this;
+        _this.initData.splice(0);
+        _this.state.dataSource = [];
+        _this.state.dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        const dataBlob = {};
+        var PageNo = this.state.defaultPageNo;
+        var param = {
+            "method": 'getMoralEducationInfoList',
+            "clazzId": classId,
+            "pageNo": PageNo,
+        };
+        console.log("param", param)
+        WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
+            onResponse: function (result) {
+                console.log("assasa",result);
+                if (result.msg == '调用成功' && result.success == true) {
+                    assessME.state.selectData = result.response
+                    var arr = result.response;
+                    var pager = result.pager;
+                    for (let i = 0; i < arr.length; i++) {
+                        var topic = arr[i];
+                        dataBlob[`${i}`] = topic;
+                    }
+                    var isLoading = false;
+                    if (arr.length > 0) {
+                        if (pager.pageCount == 1 && pager.rsCount < 30) {
+                            isLoading = false;
+                        } else {
+                            isLoading = true;
+                        }
+                    } else {
+                        isLoading = false;
+                    }
+                    _this.initData = _this.initData.concat(arr);
+                    _this.setState({
+                        dataSource: _this.state.dataSource.cloneWithRows(_this.initData),
+                        isLoadingLeft: isLoading,
+                        refreshing: false
+                    })
+                } else {
+                    Toast.fail(result.msg, 1);
+                }
+            },
+            onError: function (error) {
+                Toast.info(error);
+            }
+        });
+    }
+
+
+   
+
+    /**
+     *  ListView数据全部渲染完毕的回调
+     */
+    onEndReached = (event) => {
+        var _this = this;
+        var currentPageNo = this.state.defaultPageNo;
+        if (!this.state.isLoadingLeft && !this.state.hasMore) {
+            return;
+        }
+        currentPageNo += 1;
+        this.setState({ isLoadingLeft: true, defaultPageNo: currentPageNo });
+        _this.getMoralEducationInfoList(_this.state.classId);
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(this.initData),
+            isLoadingLeft: true,
+        });
+    };
+
+    onRefresh = () => {
+        var divPull = document.getElementsByClassName('am-pull-to-refresh-content');
+        divPull[0].style.transform = "translate3d(0px, 30px, 0px)";   //设置拉动后回到的位置
+        this.setState({ defaultPageNo: 1, refreshing: true, isLoadingLeft: true });
+        this.getMoralEducationInfoList(this.state.classId);
+    }
+
+    /**
+     * 跳转获取ClassId页面
+     * @param {*} item 
+     */
+
+    // toAssessMoralE(item){
+    //     var url = WebServiceUtil.mobileServiceURL + "assessMoralEducation?classId="+item.defaultBindedClazz.id;
+    //     var data = {
+    //         method: 'openNewPage',
+    //         url: url
+    //     };
+    //     Bridge.callHandler(data, null, function (error) {
+    //         window.location.href = url;
+    //     });
+    // }
+    /**
+     * toUpdateMoralEducatio跳转修改页面
+     */
+    toUpdateMoralEducation(item) {
+
+        var url = WebServiceUtil.mobileServiceURL + "updateMoralEducation?id=" + item.id;
+        var data = {
+            method: 'openNewPage',
+            url: url
+        };
+        Bridge.callHandler(data, null, function (error) {
+            window.location.href = url;
+        });
+
+
+
+    }
+    /**
+     * 根据ID删除德育信息
+     */
+    delMoralEducation(id) {
+            var _this = this;
+            var param = {
+                "method": 'deleteMoralEducation',
+                "id": id,
+            };
+            WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
+                onResponse: (result) => {
+                    if (result.msg == '调用成功' || result.success == true) {
+                        Toast.success('删除成功', 1);
+                        _this.state.dataSource = [];
+                        _this.state.dataSource = new ListView.DataSource({
+                            rowHasChanged: (row1, row2) => row1 !== row2,
+                        });
+                        _this.initData.forEach(function (v, i) {
+                            if (id == v.id) {
+                                _this.initData.splice(i, 1);
+                            }
+                        });
+                        _this.setState({
+                            dataSource: _this.state.dataSource.cloneWithRows(_this.initData)
+                        });
+                    } else {
+                        Toast.fail(result.msg)
+                    }
+    
+                },
+                onError: function (error) {
+                    Toast.info('删除失败');
+                }
+            });
+    }
+    /**
+     * searchClassroomName搜索班级的名称
+     */
+    toaddMoralEducation() {
+        var url = WebServiceUtil.mobileServiceURL + "addMoralEducation?classId="+assessME.state.classId;
+        var data = {
+            method: 'openNewPage',
+            url: url
+        };
+
+        Bridge.callHandler(data, null, function (error) {
+            window.location.href = url;
+        });
+    }
+    render() {
+        var _this = this;
+        const row = (rowData, sectionID, rowID) => {
+            return (<div>
+                {
+                    <div className="classInfo">
+                        <div className="textOver">
+                            <span className="classroom">{rowData.clazz.name}</span>
+                            <span className="healthScore">{rowData.health}</span>
+                            <span className="politenessScore">{rowData.politeness}</span>
+                            <span className="createTime">{WebServiceUtil.formatYMD(rowData.createTime)}</span>
+                        </div>
+
+                        <span className='calmCardUnbind' onClick={this.toUpdateMoralEducation.bind(this, rowData)}>修改</span>
+                        <span className='' onClick={this.delMoralEducation.bind(this, rowData.id)}>删除</span>
+                    </div>
+                }
+            </div>
+
+            )
+        };
+        return (
+            <div id="classroomManage" style={{ height: assessME.state.clientHeight }}>
+                <div className='tableDiv' style={{ height: assessME.state.clientHeight }}>
+                    {/*这是列表数据,包括添加按钮*/}
+                    <ListView
+                        ref={el => this.lv = el}
+                        dataSource={this.state.dataSource}    //数据类型是 ListViewDataSource
+                        renderFooter={() => (
+                            <div style={{ paddingTop: 5, paddingBottom: 40, textAlign: 'center' }}>
+                                {this.state.isLoadingLeft ? '正在加载' : '已经全部加载完毕'}
+                            </div>)}
+                        renderRow={row}   //需要的参数包括一行数据等,会返回一个可渲染的组件为这行数据渲染  返回renderable
+                        className="am-list"
+                        pageSize={30}    //每次事件循环（每帧）渲染的行数
+                        //useBodyScroll  //使用 html 的 body 作为滚动容器   bool类型   不应这么写  否则无法下拉刷新
+                        scrollRenderAheadDistance={200}   //当一个行接近屏幕范围多少像素之内的时候，就开始渲染这一行
+                        onEndReached={this.onEndReached}  //当所有的数据都已经渲染过，并且列表被滚动到距离最底部不足onEndReachedThreshold个像素的距离时调用
+                        onEndReachedThreshold={10}  //调用onEndReached之前的临界值，单位是像素  number类型
+                        initialListSize={30}   //指定在组件刚挂载的时候渲染多少行数据，用这个属性来确保首屏显示合适数量的数据
+                        scrollEventThrottle={20}     //控制在滚动过程中，scroll事件被调用的频率
+                        style={{
+                            height: assessME.state.clientHeight,
+                        }}
+                        pullToRefresh={<PullToRefresh
+                            onRefresh={this.onRefresh}
+                            distanceToRefresh={80}
+                        />}
+                    />
+                    <div className='addCourseButton'>
+                        <WhiteSpace size="lg" />
+                        <WingBlank>
+                            <Button type="warning" onClick={this.binding}>提交</Button>
+                        </WingBlank>
+                    </div>
+                    {/* <div className='addBunton' onClick={this.toaddMoralEducation}>
+                        <img src={require("../imgs/addBtn.png")} />
+                    </div> */}
+                     <div className='addBunton' onClick={this.toaddMoralEducation}>
+                        <img src={require("../imgs/addBtn.png")} />
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
--- src/components/classCardSystemBackstage/js/updateClassroom.jsx
+++ src/components/classCardSystemBackstage/js/updateClassroom.jsx
@@ -4,17 +4,12 @@ import {
     InputItem,
     List,
     Radio,
-    ListView,
-    Modal,
     PullToRefresh,
     Checkbox, Flex
 } from 'antd-mobile';
 import '../css/updateClassroom.less'
 import { ucs2 } from 'punycode';
 
-const CheckboxItem = Checkbox.CheckboxItem;
-const AgreeItem = Checkbox.AgreeItem;
-const alert = Modal.alert;
 const RadioItem = Radio.RadioItem;
 var updateCM;
 
@@ -23,24 +18,19 @@ export default class updateClassroom extends React.Component {
     constructor(props) {
         super(props);
         updateCM = this;
-        const dataSource = new ListView.DataSource({
-            rowHasChanged: (row1, row2) => row1 !== row2,
-        });
-        this.initData = [];
         this.state = {
-            dataSource: dataSource.cloneWithRows(this.initData),
-            defaultPageNo: 1,
             clientHeight: document.body.clientHeight,
             chooseResultDiv: 'none',
             searchData: [],
             selectData: [],
-            calmFlag:false
+            calmHeight: document.body.clientHeight - 190 
         };
     }
-   
-    onDataChange = (value) => {
+
+    onDataChange = (value, id) => {
         updateCM.setState({
-            gradeNameValue:value
+            gradeNameValue: value,
+            "classId": id
         });
     };
 
@@ -49,16 +39,14 @@ export default class updateClassroom extends React.Component {
         var locationSearch = locationHref.substr(locationHref.indexOf("?") + 1);
         var classIdBynoom = locationSearch.split("&")[0].split("=")[1];
         this.viewClassRoom(classIdBynoom);
-        this.setState({classIdBynoom});
+        this.setState({ classIdBynoom });
     }
-   
 
     componentDidMount() {
         Bridge.setShareAble("false");
         document.title = '绑定教室信息';
         var uid = JSON.parse(localStorage.getItem("uIdKey")).uidKey;
         this.setState({ "uid": uid })
-        this.viewClassRoomPage(uid);
         //添加对视窗大小的监听,在屏幕转换以及键盘弹起时重设各项高度
         window.addEventListener('resize', updateCM.onWindowResize)
     }
@@ -67,8 +55,11 @@ export default class updateClassroom extends React.Component {
         //解除监听
         window.removeEventListener('resize', updateCM.onWindowResize)
     }
-
-    viewClassRoom(roomId){
+    /**
+     * 根据教室ID显示对应的信息
+     * @param {*} roomId 
+     */
+    viewClassRoom(roomId) {
         var _this = this;
         var param = {
             "method": 'viewClassRoom',
@@ -79,77 +70,30 @@ export default class updateClassroom extends React.Component {
                 if (result.msg == '调用成功' && result.success == true) {
                     var clazzRoom = result.response
                     var roomName = clazzRoom.name;
-                    _this.setState({'classroomValue':roomName});
+                    var gradeName = clazzRoom.defaultBindedClazz.name;
+                    var defaultId = clazzRoom.defaultBindedClazz.id;
+                    _this.setState({
+                        'classroomValue': roomName,
+                        "gradeNameValue": gradeName,
+                        "classId":defaultId
+                    });
+                } else {
+                    Toast.fail(result.msg, 1);
                 }
             },
             onError: function (error) {
-                // message.error(error);
+                message.error(error);
             }
         });
     }
-
     /**
      * 视窗改变时改变高度
      */
     onWindowResize() {
         setTimeout(function () {
-            updateCM.setState({ clientHeight: document.body.clientHeight });
+            updateCM.setState({ clientHeight: document.body.clientHeight,calmHeight: document.body.clientHeight - 190  });
         }, 100)
     }
-
-    /**
-     * 展示班级页面信息
-     */
-    viewClassRoomPage(uid) {
-        var _this = this;
-        _this.initData.splice(0);
-        _this.state.dataSource = [];
-        _this.state.dataSource = new ListView.DataSource({
-            rowHasChanged: (row1, row2) => row1 !== row2,
-        });
-        const dataBlob = {};
-        var PageNo = this.state.defaultPageNo;
-        var param = {
-            "method": 'viewClassRoomPage',
-            "uid": uid,
-            "pn": PageNo,
-        };
-        WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
-            onResponse: function (result) {
-                if (result.msg == '调用成功' && result.success == true) {
-                    updateCM.state.selectData = result.response
-                    var arr = result.response;
-                    var pager = result.pager;
-                    for (let i = 0; i < arr.length; i++) {
-                        var topic = arr[i];
-                        dataBlob[`${i}`] = topic;
-                    }
-                    var isLoading = false;
-                    if (arr.length > 0) {
-                        if (pager.pageCount == 1 && pager.rsCount < 30) {
-                            isLoading = false;
-                        } else {
-                            isLoading = true;
-                        }
-                    } else {
-                        isLoading = false;
-                    }
-                    _this.initData = _this.initData.concat(arr);
-                    _this.setState({
-                        dataSource: _this.state.dataSource.cloneWithRows(_this.initData),
-                        isLoadingLeft: isLoading,
-                        refreshing: false
-                    })
-                }
-            },
-            onError: function (error) {
-                // message.error(error);
-            }
-        });
-    }
-
-
-
     /**
      * searchClassroomName搜索班级的名称
      */
@@ -158,22 +102,22 @@ export default class updateClassroom extends React.Component {
         var param = {
             "method": 'searchClazz',
             "aid": updateCM.state.uid,
-            "keyWord": $('.gradeName .am-input-control input').val(),
+            "keyWord":updateCM.state.gradeNameValue,
         };
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: function (result) {
                 if (result.msg == '调用成功' && result.success == true) {
-
                     updateCM.setState({
-                        searchData: result.response,
                         chooseResultDiv: "block",
-                        classId: result.response[0].id
+                        searchData: result.response,
+                        classId: updateCM.state.classId
                     })
                 } else {
                     Toast.fail(result.msg, 1);
                 }
             },
             onError: function (error) {
+                message.error(error);
             }
         });
     }
@@ -186,31 +130,15 @@ export default class updateClassroom extends React.Component {
             Toast.fail('请填写教室名称和班级名称', )
             return
         }
-        var param;
-
-        if(this.state.calmFlag) {
-            param = {
-                "method": 'updateClassRoom',
-                "cr": {
-                    "creatorId": updateCM.state.uid,
-                    "name": updateCM.state.classroomValue,
-                    "classId": updateCM.state.classId,
-                    "id":updateCM.state.classIdBynoom
-                }
-    
-            };
-        } else{
-            param = {
-                "method": 'updateClassRoom',
-                "cr": {
-                    "creatorId": updateCM.state.uid,
-                    "name": updateCM.state.classroomValue,
-                    "id":updateCM.state.classIdBynoom
-                }
-    
-            };
-        }
-    
+        var param = {
+            "method": 'updateClassRoom',
+            "cr": {
+                "creatorId": updateCM.state.uid,
+                "name": updateCM.state.classroomValue,
+                "classId": updateCM.state.classId,
+                "id": updateCM.state.classIdBynoom
+            }
+        };
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: function (result) {
                 if (result.msg == '调用成功' && result.success == true) {
@@ -218,17 +146,11 @@ export default class updateClassroom extends React.Component {
                     _this.state.gradeNameValue = '';
                     _this.state.classroomValue = '';
                     _this.setState({ chooseResultDiv: 'none' });
-                    _this.viewClassRoomPage(_this.state.uid);
-                    $('.bindGrade span').removeClass("am-checkbox-checked");
-                    $('.gradeName').css({
-                        display: 'none'
-                    })
                     Toast.success('添加成功');
                     setTimeout(function () {
                         var data = {
                             method: 'finishForRefresh',
                         };
-
                         Bridge.callHandler(data, null, function (error) {
                         });
                     }, 1000)
@@ -243,27 +165,6 @@ export default class updateClassroom extends React.Component {
 
     }
 
-
-    /**
-     * 获取绑定班级的状态，是否显示
-     */
-    getbindGradeState(e) {
-        if (e.target.checked) {
-            this.setState({calmFlag:true,
-            gradeNameValue:''})
-            $('.gradeName').css({
-                display: 'block'
-            })
-        } else {
-            this.setState({calmFlag:false,
-                chooseResultDiv:'none'
-            })
-            $('.gradeName').css({
-                display: 'none'
-            })
-            
-        }
-    }
     /**
      * 输入框改变的回调
      */
@@ -273,41 +174,13 @@ export default class updateClassroom extends React.Component {
     inputChange(e) {
         this.setState({ gradeNameValue: e })
     }
-    
-    /**
-     *  ListView数据全部渲染完毕的回调
-     */
-    onEndReached = (event) => {
-        var _this = this;
-        var currentPageNo = this.state.defaultPageNo;
-        if (!this.state.isLoadingLeft && !this.state.hasMore) {
-            return;
-        }
-        currentPageNo += 1;
-        this.setState({ isLoadingLeft: true, defaultPageNo: currentPageNo });
-        _this.viewClassRoomPage(_this.state.uid);
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(this.initData),
-            isLoadingLeft: true,
-        });
-    };
 
-    onRefresh = () => {
-        var divPull = document.getElementsByClassName('am-pull-to-refresh-content');
-        divPull[0].style.transform = "translate3d(0px, 30px, 0px)";   //设置拉动后回到的位置
-        this.setState({ defaultPageNo: 1, refreshing: true, isLoadingLeft: true });
-        this.viewClassRoomPage(this.state.uid);
-    }
-   
     render() {
-       
         var _this = this;
-       
         return (
             <div id="updateClassroom" style={{ height: updateCM.state.clientHeight }}>
                 <div className='addModel' style={{ height: updateCM.state.clientHeight }}>
                     <List>
-
                         <div className='classroomName'>
                             <InputItem
                                 placeholder="请输入教室名称"
@@ -316,12 +189,7 @@ export default class updateClassroom extends React.Component {
                                 value={this.state.classroomValue}
                             >教室名称<i className='redStar'>*</i></InputItem>
                         </div>
-                        <div className="bindGrade">
-                            <AgreeItem data-seed="logId" onChange={e => this.getbindGradeState(e)}>
-                                绑定班级
-                            </AgreeItem>
-                        </div>
-                        <div className='gradeName' style={{ display: "none" }}>
+                        <div className='gradeName'>
                             <InputItem
                                 placeholder="请输入班级名称"
                                 data-seed="logId"
@@ -329,16 +197,14 @@ export default class updateClassroom extends React.Component {
                                 value={this.state.gradeNameValue}
                             >班级名称<i className='redStar'>*</i></InputItem>
                             <div id='stIcon' className='stIcon' onClick={this.searchClassroomName}>
-                                <img  src={require('../imgs/icon_search.png')}/>
+                                <img src={require('../imgs/icon_search.png')} />
                             </div>
-
-
                         </div>
                         <div className='chooseResult'
-                            style={{ display: this.state.chooseResultDiv }}>
+                            style={{ display: this.state.chooseResultDiv,height: this.state.calmHeight  }}>
                             <List>
                                 {updateCM.state.searchData.map(i => (
-                                    <RadioItem key={i.id} checked={ updateCM.state.gradeNameValue === i.name}  onChange={() => this.onDataChange(i.name)}>
+                                    <RadioItem key={i.id} checked={updateCM.state.gradeNameValue === i.name} onChange={() => this.onDataChange(i.name, i.id)}>
                                         {i.name}
                                     </RadioItem>
                                 ))}
@@ -348,7 +214,6 @@ export default class updateClassroom extends React.Component {
                     <div className="bottomBox submitBtn">
                         <span className="bind" onClick={this.binding}>提 交</span>
                     </div>
-
                 </div>
             </div>
         );
