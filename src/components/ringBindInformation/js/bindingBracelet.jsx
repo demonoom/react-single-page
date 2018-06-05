155648bed2f617a1c631ecb00e9351f60dba08e6
noom <noom0130@foxmail.com>
2018-06-05 21:05:54 +0800

    .

--- src/components/ringBindInformation/js/bindingBracelet.jsx
+++ src/components/ringBindInformation/js/bindingBracelet.jsx
@@ -197,6 +197,10 @@ export default class bindingBracelet extends React.Component {
         Bridge.callHandler(data, function (mes) {
             //获取二维码MAC地址
             var string = mes.replace(/:/g, '');
+            if (string.length > 16) {
+                Toast.fail('mac地址超过最大字节数', 2)
+                return
+            }
             bindDing.setState({macId: string});
         }, function (error) {
             console.log(error);
