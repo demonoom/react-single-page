6346f613365d6a0c130ebf26775d4455f3c326d0
王鹏飞 <398135290@qq.com>
2018-06-06 11:04:16 +0800

    ----

--- src/components/ringBindInformation/js/boxBracelet.jsx
+++ src/components/ringBindInformation/js/boxBracelet.jsx
@@ -189,11 +189,12 @@ export default class boxBracelet extends React.Component {
         };
         Bridge.callHandler(data, function (mes) {
             //获取二维码MAC地址
-            var string = mes.replace(/:/g, '');
-            if (string.length > 16) {
-                Toast.fail('mac地址超过最大字节数', 2)
-                return
-            }
+            //var string = mes.replace(/:/g, '');
+            var string = mes;
+            //if (string.length > 16) {
+            //    Toast.fail('mac地址超过最大字节数', 2)
+            //    return
+            //}
             bindDing.setState({macId: string});
         }, function (error) {
             console.log(error);
