4c7ceebe19ac9aa3f4a830c20494e647e779504e
noom <noom0130@foxmail.com>
2018-06-13 15:29:26 +0800

    .

--- src/components/charts/dashboard/dashboard.jsx
+++ src/components/charts/dashboard/dashboard.jsx
@@ -665,7 +665,6 @@ export default class dashboard extends React.Component {
 
         var xClazzNameArray = [];
         var seriesDataArray = [];
-        console.log(braceletSportSteps);
         braceletSportSteps.forEach(function (braceletSportStepObj) {
             var clazzName = braceletSportStepObj.clazz.grade.name + '' + braceletSportStepObj.clazz.name;
             var sportStep = braceletSportStepObj.sportStep;
@@ -1038,7 +1037,6 @@ export default class dashboard extends React.Component {
         var xAttendanceNameArray = [];
         var ySeriesDataArray = [];
         if (WebServiceUtil.isEmpty(attendanceResults) == false) {
-            console.log(attendanceResults);
             var absent = attendanceResults["absent"];
             var early = attendanceResults["early"];
             var late = attendanceResults["late"];
