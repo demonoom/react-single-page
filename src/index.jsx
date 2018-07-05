4a557f79def7c2d8bb374271af167e4793bc36a7
songyongfeng <446942919@qq.com>
2018-07-05 17:11:02 +0800

    .

--- src/index.jsx
+++ src/index.jsx
@@ -739,10 +739,6 @@ class Index extends React.Component {
                     {/*<li><Link*/}
                     {/*to="/particlePath"*/}
                     {/*style={{fontSize: '24px'}}>运动轨迹</Link></li>*/}
-
-                    {/*<li><Link*/}
-                        {/*to="/wxLogin?local=http://jiaoxue.maaee.com:8091/#/wxBindIndex"*/}
-                        {/*style={{ fontSize: '24px' }}>授权登录至微信绑定页面</Link></li>*/}
                     <li><Link
                         to="/wxLogin?local=wxBindIndex"
                         style={{ fontSize: '24px' }}>授权登录至微信绑定页面</Link></li>
