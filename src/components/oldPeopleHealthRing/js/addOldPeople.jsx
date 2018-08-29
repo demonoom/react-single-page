02e1ea75a433d216e80d78de1a1cefbc21196797
calm <calm420@163.com>
2018-08-29 18:59:53 +0800

    .

--- src/components/oldPeopleHealthRing/js/addOldPeople.jsx
+++ src/components/oldPeopleHealthRing/js/addOldPeople.jsx
@@ -107,8 +107,12 @@ export default class addOldPeople extends React.Component {
             needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
             success: function (res) {
+                if (res.resultStr.indexOf(":") == -1) {
+                    var string = splitStrTo2(res.resultStr).join(":");
+                    mes = string.substr(0, string.length - 1)
+                }
                 calm.setState({
-                    macId: res.resultStr
+                    macId: mes.toUpperCase()
                 })
                 // var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
             }
