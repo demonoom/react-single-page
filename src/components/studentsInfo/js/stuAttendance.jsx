dde6adcbc68e24f944f0fd4da96b4ef5e07151fa
zhaojuan <343524032@qq.com>
2018-09-29 14:09:13 +0800

    -

--- src/components/studentsInfo/js/stuAttendance.jsx
+++ src/components/studentsInfo/js/stuAttendance.jsx
@@ -59,6 +59,9 @@ export default class stuAttendance extends React.Component {
             onResponse: (res) => {
                 console.log(res, 'res');
                 if (res.success == true && res.msg == '调用成功') {
+                    this.initData = [{
+                        value: -1,
+                    }]
                     this.initData = this.initData.concat(res.response);
                     this.setState({
                         dataSource: dataSource.cloneWithRows(this.initData),
@@ -98,7 +101,7 @@ export default class stuAttendance extends React.Component {
                     })
                     this.setState({
                         clazzId: [res.response[0].id],
-                        className: res.response[0].grade.name + res.response[0].name
+                        className: [res.response[0].grade.name + res.response[0].name]
                     }, () => {
                         if (callback) {
                             callback();
@@ -143,13 +146,15 @@ export default class stuAttendance extends React.Component {
         });
     }
 
-    onOk = (value) => {
+    onOk = (value,label) => {
+        // console.log(label)
         if (value.length <= 0) {
             Toast.info('未选中任何班级!', 1);
             return;
         }
         this.setState({
             clazzId: value,
+            className: ['是是是','ssss'],
         }, () => {
             this.getBraceletStudentInfoAttendByClazzId();
             this.onCancel();
@@ -177,43 +182,36 @@ export default class stuAttendance extends React.Component {
 
 
     render() {
+
         const row = (rowData, sectionID, rowID) => {
-            return (
-                <Item align="top" thumb="http://i2.hdslb.com/bfs/face/91e4fa4006e6af4801da253640128d59bcebe1e6.jpg"
-                      multipleLine>
-                    {rowData.user.userName} <Brief>本周迟到{rowData.attend.weekCount}次&nbsp;
-                    本月迟到{rowData.attend.monthCount}次</Brief>
-                </Item>
-            )
-        };
-        const {initData, show} = this.state;
-        const menuEl = (
-            <Menu
-                className="single-multi-foo-menu"
-                data={initData}
-                value={this.state.clazzId}
-                level={1}
-                onChange={this.onChange}
-                onOk={this.onOk}
-                onCancel={this.onCancel}
-                height={document.documentElement.clientHeight * 0.6}
-                multiSelect
-            />
-        );
-        const loadingEl = (
-            <div style={{
-                position: 'absolute',
-                width: '100%',
-                height: document.documentElement.clientHeight * 0.6,
-                display: 'flex',
-                justifyContent: 'center'
-            }}>
-                <ActivityIndicator size="large"/>
-            </div>
-        );
-        return (
-            <div id="stuAttendance">
-                <div style={
+            var dom = '';
+            if(rowData.value == -1){
+                const {initData, show} = this.state;
+                const menuEl = (
+                    <Menu
+                        className="single-multi-foo-menu stuModal"
+                        data={initData}
+                        value={this.state.clazzId}
+                        level={1}
+                        onChange={this.onChange}
+                        onOk={this.onOk}
+                        onCancel={this.onCancel}
+                        height={document.documentElement.clientHeight * 0.6}
+                        multiSelect
+                    />
+                );
+                const loadingEl = (
+                    <div style={{
+                        position: 'absolute',
+                        width: '100%',
+                        height: document.documentElement.clientHeight * 0.6,
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <ActivityIndicator size="large"/>
+                    </div>
+                );
+                dom = <div style={
                     this.state.type == 'TEAC' ? {display: 'block'} : {display: 'none'}
                 } className={show ? 'single-multi-menu-active' : ''}>
                     <div>
@@ -222,7 +220,9 @@ export default class stuAttendance extends React.Component {
                             mode="light"
                             onClick={this.handleClick}
                             className="single-multi-top-nav-bar"
-                            rightContent={this.state.className}
+                            rightContent={this.state.className.map((value,index)=>{
+                                return <span>{value}</span>
+                            })}
                         >
                             {/*请选择班级*/}
                         </NavBar>
@@ -230,6 +230,21 @@ export default class stuAttendance extends React.Component {
                     {show ? initData ? menuEl : loadingEl : null}
                     {show ? <div className="menu-mask" onClick={this.onCancel}/> : null}
                 </div>
+            }else{
+                dom = <Item align="top" thumb="http://i2.hdslb.com/bfs/face/91e4fa4006e6af4801da253640128d59bcebe1e6.jpg"
+                            multipleLine>
+                    {rowData.user.userName} <Brief>本周迟到{rowData.attend.weekCount}次&nbsp;
+                    本月迟到{rowData.attend.monthCount}次</Brief>
+                </Item>
+            }
+            return (
+                dom
+            )
+        };
+
+        return (
+            <div id="stuAttendance">
+
                 <ListView
                     ref={el => this.lv = el}
                     dataSource={this.state.dataSource}    //数据类型是 ListViewDataSource
@@ -238,7 +253,7 @@ export default class stuAttendance extends React.Component {
                     //         {this.state.isLoading ? '正在加载...' : '已经全部加载完毕'}
                     //     </div>)}
                     renderRow={row}   //需要的参数包括一行数据等,会返回一个可渲染的组件为这行数据渲染  返回renderable
-                    className="am-list"
+                    className="am-list stuAttendanceList"
                     pageSize={30}    //每次事件循环（每帧）渲染的行数
                     //useBodyScroll  //使用 html 的 body 作为滚动容器   bool类型   不应这么写  否则无法下拉刷新
                     scrollRenderAheadDistance={200}   //当一个行接近屏幕范围多少像素之内的时候，就开始渲染这一行
@@ -247,7 +262,7 @@ export default class stuAttendance extends React.Component {
                     initialListSize={30}   //指定在组件刚挂载的时候渲染多少行数据，用这个属性来确保首屏显示合适数量的数据
                     scrollEventThrottle={20}     //控制在滚动过程中，scroll事件被调用的频率
                     style={{
-                        height: document.body.clientHeight,
+                        height: document.body.clientHeight - 45,
                     }}
                 />
             </div>
