e04e154997f0e0cdda43cd06fb3da6ffa1ba0294
王鹏飞 <398135290@qq.com>
2018-11-14 23:03:18 +0800

    打包时候自动更新版本号

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,9 @@
 const path = require('path');  //引入path模块
 const webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
-const pxtorem = require('postcss-pxtorem');
-
-const Visualizer = require('webpack-visualizer-plugin'); // remove it in production environment.
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; // remove it in production environment.
-const otherPlugins = process.argv[1].indexOf('webpack-dev-server') >= 0 ? [] : [
-    new Visualizer(), // remove it in production environment.
-    new BundleAnalyzerPlugin({
-        defaultSizes: 'parsed',
-        // generateStatsFile: true,
-        statsOptions: {source: false}
-    }), // remove it in production environment.
-];
+const version=new Date().getTime();
 
 const postcssOpts = {
     ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
@@ -22,7 +11,6 @@ const postcssOpts = {
         autoprefixer({
             browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
         }),
-        // pxtorem({ rootValue: 100, propWhiteList: [] })
     ],
 };
 
@@ -44,10 +32,9 @@ module.exports = {
     }, /*指向spa应用的入口文件*/
 
     output: {
-        filename: '[name].js',
-        chunkFilename: '[id].chunk.js?v=0.1.2',   //匹配chunk
+        filename: '[name].js?v='+version,
+        chunkFilename: '[id].chunk.js?v='+version,   //匹配chunk
         path: path.join(__dirname, '/dist'), /*输出的文件路径*/
-        //publicPath: '/dist/' 修改的地方
         publicPath: '/'
     },
 
@@ -105,31 +92,12 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.optimize.CommonsChunkPlugin('shared'),
+        //new webpack.optimize.CommonsChunkPlugin('shared'),
         //分离第三方应用的插件
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     // minChunks: 2,
-        //     name: 'vendor',
-        //     filename: 'shared.js'
-        // }),
-
-        // new webpack.optimize.UglifyJsPlugin({
-        //     //压缩插件,使用npm 安装, cnpm会报错
-        //     mangle: {
-        //         except: ['$super', '$', 'exports', 'require', 'module', '_']
-        //     },
-        //     output: {
-        //         // 是否输出可读性较强的代码，即会保留空格和制表符，默认为是，为了达到更好的压缩效果，可以设置为 false。
-        //         beautify: false,
-        //         // 是否保留代码中的注释，默认为保留，为了达到更好的压缩效果，可以设置为 false。
-        //         comments: false,
-        //     },
-        //     compress: {
-        //         warnings: false,
-        //         // 是否剔除代码中所有的  console  语句，默认为不剔除。开启后不仅可以提升代码压缩效果，也可以兼容不支持 console 语句 IE 浏览器。
-        //         drop_console: true,
-        //     }
-        // }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'shared.js?v='+version
+        }),
 
         //将开发模式变为生产模式
         new webpack.DefinePlugin({
@@ -138,24 +106,14 @@ module.exports = {
             },
         }),
         //抽取CSS文件插件
-        new ExtractTextPlugin({filename: '[name].css', allChunks: true}),
-        // new HtmlWebpackPlugin({
-        //     hash: true,
-        //     filename: 'index.html'
-        // })
+        new ExtractTextPlugin({filename: '[name].css?v='+version, allChunks: true}),
 
         new HtmlWebpackPlugin({
             title: "HtmlPlugin",
-            // filename :"index.html",
             template: path.join(__dirname, "./index_deploy.html"),
-            // template:(useDefinedHtml ? useDefinedHtml : defaultHtml),
-            //we must use html-loader here instead of file-loader
             inject: "body",
             cache: false,
             xhtml: false
         })
-
-
-        //...otherPlugins
     ]
 }
