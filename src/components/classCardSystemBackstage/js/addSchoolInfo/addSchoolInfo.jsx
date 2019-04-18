c92cf3d783aa486bf1a5bf927e3f2c5bc8e6f055
calm <calm420@163.com>
2019-04-18 17:17:23 +0800

    .

--- src/components/classCardSystemBackstage/js/addSchoolInfo/addSchoolInfo.jsx
+++ src/components/classCardSystemBackstage/js/addSchoolInfo/addSchoolInfo.jsx
@@ -92,6 +92,14 @@ export default class addSchoolInfo extends React.Component {
 
     //保存学校简介
     toSaveSchoolInfo = () => {
+        if(this.state.schoolInfo == ""){
+            Toast.info("学校简介不能为空哦",1)
+            return
+        }
+        if(this.state.teaPicSrc == ""){
+            Toast.info("学校校徽不能为空哦",1)
+            return
+        }
         var param = {
             "method": 'updateSchool',
             "id": this.state.schoolId,
@@ -99,6 +107,7 @@ export default class addSchoolInfo extends React.Component {
             "avatar":this.state.teaPicSrc,
             "actionName": "sharedClassAction",
         };
+        console.log(JSON.stringify(param),"param")
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: (result) => {
                 console.log(result, "result")
